a734671ae69d1c5f7cf8f5f8d86325b9d318feb1
Chris Chaffey <chris.chaffey@gmail.com>
2021-09-19 23:01:15 -0300

    Fixing compiler issue

--- kyra/engine/imagetree.h
+++ kyra/engine/imagetree.h
@@ -190,7 +190,7 @@ class KrImageTree
 	void Clear( KrImNode* root );	// delete the entire tree
 
 	// Recursive hit test walk.
-	bool KrImageTree::HitTestRec( KrImNode* node, int x, int y, int flags, std::vector<KrImage*>* outputArray, int windowIndex );
+	bool HitTestRec( KrImNode* node, int x, int y, int flags, std::vector<KrImage*>* outputArray, int windowIndex );
 
 	KrImNode*  root;			// The root to position the window
 	KrImNode*  offsetRoot;		// The root as returned to the client
